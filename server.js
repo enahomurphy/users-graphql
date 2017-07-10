63e09e34b39f0849bf3b9d70f2fdab0c02a25e47
enaho murphy <enahpmurphy@saitama.local>
2017-07-10 23:56:25 +0100

    add graphql server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-node_modules
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true,
+}));
+
+
+app.listen(4000);
